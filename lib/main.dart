add50d1dbee4ef199bc61636a21bf14220a59b41
Alice Anglesjö <alice.anglesjo@hotmail.com>
2022-05-08 14:47:41 +0200

    fix CustomMultiChildLayout and lines between circles

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,153 @@
 import 'package:flutter/material.dart';
-import 'package:petli/circle/circle.dart';
-import 'package:petli/shape/shape.dart';
-import 'package:petli/triangle/triangle.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  final c1 = const Circle();
-  static const c2 = Circle();
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              backgroundColor: const Color(0xff5c3c61),
-              title: const Text(
-                'Petli Assignment 🐶',
-                style: TextStyle(color: Colors.white),
+    return const MaterialApp(
+      home: MyCanvas(),
+    );
+  }
+}
+
+class MyCanvas extends StatefulWidget {
+  const MyCanvas({Key? key}) : super(key: key);
+
+  @override
+  _MyCanvasState createState() => _MyCanvasState();
+}
+
+class Position {
+  double _x;
+  double _y;
+
+  Position(this._x, this._y);
+
+  setPosition(double x, double y) {
+    _x = x;
+    _y = y;
+  }
+
+  double get x {
+    return _x;
+  }
+
+  double get y {
+    return _y;
+  }
+}
+
+class _MyCanvasState extends State<MyCanvas> {
+  var pos = <Position>[];
+
+  @override
+  Widget build(BuildContext context) {
+    pos.add(Position(192, 476));
+    pos.add(Position(312, 253));
+    pos.add(Position(52, 253));
+    pos.add(Position(50, 50));
+
+    return Scaffold(
+      body: Center(
+        child: CustomMultiChildLayout(
+          delegate: DragArea(pos),
+          children: <Widget>[
+            LayoutId(
+              id: 't0',
+              child: Draggable(
+                feedback: const Icon(Icons.circle_outlined),
+                child: const Icon(
+                  Icons.circle_outlined,
+                  color: Color(0xffff99af),
+                ),
+                childWhenDragging: Container(),
+                onDragEnd: (DraggableDetails d) {
+                  setState(() {
+                    pos[0].setPosition(d.offset.dx, d.offset.dy);
+                  });
+                },
               ),
             ),
-            body: CustomMultiChildLayout(
-              delegate: DragArea(),
-              children: <Widget>[
-                LayoutId(
-                  id: 't0',
-                  child: Draggable(
-                    feedback: Icon(Icons.filter_vintage),
-                    child: const Icon(
-                      Icons.ac_unit,
-                      color: Colors.green,
-                    ),
-                    childWhenDragging: Container(),
-                    onDragEnd: (DraggableDetails d) {
-                      print(d);
-                    },
-                  ),
+            LayoutId(
+              id: 't1',
+              child: Draggable(
+                feedback: const Icon(Icons.circle_outlined),
+                child: const Icon(
+                  Icons.circle_outlined,
+                  color: Color(0xffff99af),
                 ),
-              ],
-            )));
+                childWhenDragging: Container(),
+                onDragEnd: (DraggableDetails d) {
+                  setState(() {
+                    pos[1].setPosition(d.offset.dx, d.offset.dy);
+                  });
+                },
+              ),
+            ),
+            LayoutId(
+              id: 't2',
+              child: Draggable(
+                feedback: const Icon(Icons.circle_outlined),
+                child: const Icon(
+                  Icons.circle_outlined,
+                  color: Color(0xffff99af),
+                ),
+                childWhenDragging: Container(),
+                onDragEnd: (DraggableDetails d) {
+                  setState(() {
+                    pos[2].setPosition(d.offset.dx, d.offset.dy);
+                  });
+                },
+              ),
+            ),
+            LayoutId(id: 't3', child: CustomPaint(painter: LinePainter(pos))),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LinePainter extends CustomPainter {
+  var _positions = [];
+
+  LinePainter(this._positions);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final painter = Paint()
+      ..color = const Color(0xffff99af)
+      ..strokeWidth = 3
+      ..style = PaintingStyle.stroke;
+
+    var path = Path();
+    path.moveTo(_positions[0].x, _positions[0].y);
+    path.lineTo(_positions[1].x, _positions[1].y);
+    path.lineTo(_positions[2].x, _positions[2].y);
+    path.close();
+    canvas.drawPath(path, painter);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
 
 class DragArea extends MultiChildLayoutDelegate {
+  var _p = <Position>[];
+
+  DragArea(this._p);
+
   @override
   void performLayout(Size size) {
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 4; i++) {
       layoutChild('t' + i.toString(), BoxConstraints.loose(size));
+      positionChild('t' + i.toString(), Offset(_p[i].x, _p[i].y));
     }
   }
 
